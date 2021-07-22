856402fb467431c557c26f23f57a108c56b587f0
wangbin184035 <wangbin184035@hollysys.net>
2021-07-22 09:53:26 +0800

    修改rd命令

--- tocpp.bat
+++ tocpp.bat
@@ -1,3 +1,3 @@
-rd .\AntrlCalc\calc
+rd /s /q .\AntrlCalc\calc
 java -jar antlr4-4.8-1-complete.jar -Dlanguage=Cpp -no-listener -visitor -encoding UTF-8 -o .\AntrlCalc\calc -package calc LabeledExpr.g4
 @pause
